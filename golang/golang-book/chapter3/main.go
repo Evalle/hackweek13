c90d9f1b299e36d68d871deadf9c4b1d440ed848
evalle <shmarnev@gmail.com>
2015-12-07 14:46:24 +0100

    Add boolean algebra example

--- golang/golang-book/chapter3/main.go
+++ golang/golang-book/chapter3/main.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(len("Hello world"))
-	fmt.Println("Hello world"[1])
-	fmt.Println("Hello" + " world")
+	fmt.Println((true && false) || (false && true) || !(false && false))
 }
